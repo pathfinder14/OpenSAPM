1ed99ff528a9bc98bc4dab666fb7d0d677e8ff64
Nikolay Kozyrskiy <nik.kozyrskiy@gmail.com>
2017-04-29 02:59:18 +0300

    The parts of environment module were added

---
+++ utils/environment_properties_analyzers/functional_analyzer.py
@@ -0,0 +1,8 @@
+
+class funcional_analyzer(object):
+    """
+       This class is in charge of parsing given functions and parameters of environment
+       and providing class <environment_properties> with needed input data
+
+       TODO
+       """
