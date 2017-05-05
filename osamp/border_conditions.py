6ca6605e6bf652eb8b46ef99b56a63042d94839d
Rinat Aynulin <rinataynulin@gmail.com>
2017-05-05 21:24:52 +0300

    remove: boilerplate code

--- osamp/border_conditions.py
+++ osamp/border_conditions.py
@@ -9,25 +9,16 @@ def border_condition_1d(grid, left, right):
     # Check left border.
 
     if left == MethodNames.REFLECTION_METHOD and len(arr) > 2:
-        new_grid_element = GridElement1d(-arr[1].sigma, -1 * arr[1].velocity)
-        arrnew.append(new_grid_element)
-
-        new_grid_element = GridElement1d(-arr[0].sigma, -1 * arr[0].velocity)
-        arrnew.append(new_grid_element)
+        arrnew.append(GridElement1d(-arr[1].sigma, -1 * arr[1].velocity))
+        arrnew.append(GridElement1d(-arr[0].sigma, -1 * arr[0].velocity))
 
     elif left == MethodNames.CYCLE_METHOD and len(arr) > 2:
-        new_grid_element = GridElement1d(arr[len(arr) - 2].sigma, arr[len(arr) - 2].velocity)
-        arrnew.append(new_grid_element)
-
-        new_grid_element = GridElement1d(arr[len(arr) - 1].sigma, arr[len(arr) - 1].velocity)
-        arrnew.append(new_grid_element)
+        arrnew.append(GridElement1d(arr[len(arr) - 2].sigma, arr[len(arr) - 2].velocity))
+        arrnew.append(GridElement1d(arr[len(arr) - 1].sigma, arr[len(arr) - 1].velocity))
 
     else:
-        new_grid_element = GridElement1d(arr[1].sigma, arr[1].velocity)
-        arrnew.append(new_grid_element)
-
-        new_grid_element = GridElement1d(arr[0].sigma, arr[0].velocity)
-        arrnew.append(new_grid_element)
+        arrnew.append(GridElement1d(arr[1].sigma, arr[1].velocity))
+        arrnew.append(GridElement1d(arr[0].sigma, arr[0].velocity))
 
     # Add original points.
 
@@ -36,25 +27,16 @@ def border_condition_1d(grid, left, right):
     # Check right border.
 
     if right == MethodNames.REFLECTION_METHOD and len(arr) > 2:
-        new_grid_element = GridElement1d(- arr[len(arr) - 1].sigma, -1 * arr[len(arr) - 1].velocity)
-        arrnew.append(new_grid_element)
-
-        new_grid_element = GridElement1d(- arr[len(arr) - 2].sigma, -1 * arr[len(arr) - 2].velocity)
-        arrnew.append(new_grid_element)
+        arrnew.append(GridElement1d(- arr[len(arr) - 1].sigma, -1 * arr[len(arr) - 1].velocity))
+        arrnew.append(GridElement1d(- arr[len(arr) - 2].sigma, -1 * arr[len(arr) - 2].velocity))
 
     elif right == MethodNames.CYCLE_METHOD and len(arr) > 2:
-        new_grid_element = GridElement1d(arr[0].sigma, arr[0].velocity)
-        arrnew.append(new_grid_element)
-
-        new_grid_element = GridElement1d(arr[1].sigma, arr[1].velocity)
-        arrnew.append(new_grid_element)
+        arrnew.append(GridElement1d(arr[0].sigma, arr[0].velocity))
+        arrnew.append(GridElement1d(arr[1].sigma, arr[1].velocity))
 
     else:
-        new_grid_element = GridElement1d(arr[len(arr) - 1].sigma, arr[len(arr) - 1].velocity)
-        arrnew.append(new_grid_element)
-
-        new_grid_element = GridElement1d(arr[len(arr) - 2].sigma, arr[len(arr) - 2].velocity)
-        arrnew.append(new_grid_element)
+        arrnew.append(GridElement1d(arr[len(arr) - 1].sigma, arr[len(arr) - 1].velocity))
+        arrnew.append(GridElement1d(arr[len(arr) - 2].sigma, arr[len(arr) - 2].velocity))
 
     return Grid1d(arrnew, len(arrnew))
     # And make a new grid from this.
