f81a33c33c963d3c676d00a2173c67b40d425132
Nikolay Kozyrskiy <nik.kozyrskiy@gmail.com>
2017-05-05 22:25:02 +0300

    create_environment_for_acoustic() function in environment_properties.py was added

---
+++ environment_visualizer.py
@@ -0,0 +1,7 @@
+
+class environment_visualizer(object):
+    """
+        This class is in charge of visualizing the created by class <environment_properties> environment model
+
+        TODO
+    """
---
+++ utils/environment_properties_analyzers/visual_analyzer.py
@@ -0,0 +1,8 @@
+
+class visual_analyzer(object):
+    """
+    This class is in charge of parsing given picture model of environment
+    and providing class <environment_properties> with needed input data
+
+    TODO
+    """
