ee258894885bb7854878046bc9cdb8609c8af69e
Rinat Aynulin <rinataynulin@gmail.com>
2017-05-05 20:01:50 +0300

    add: grid for 1d

--- osamp/grid.py
+++ osamp/grid.py
@@ -1,2 +1,15 @@
-class Grid():   
-    """Generate grid/mesh."""
+class GridElement1d:
+    def __init__(self, sigma, velocity):
+        self.sigma = sigma
+        self.velocity = velocity
+
+    def set_value(self, sigma, velocity):
+        self.sigma = sigma
+        self.velocity = velocity
+
+
+class Grid1d:
+    def __init__(self, dimension):
+        self.elements = []  # list of GridElement1d
+        self.dimension = dimension
+
