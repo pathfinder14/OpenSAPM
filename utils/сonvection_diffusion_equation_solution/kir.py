58e3d7ec6bba19ad50946260bb9c48f87aad8dc3
Ilya Mogilin <ilya.mogilin@phystech.edu>
2017-05-06 04:11:58 +0300

    Created 2 methods of solving of convection-diffusiuon_equation_solution

---
+++ utils/сonvection_diffusion_equation_solution/kir.py
@@ -0,0 +1,7 @@
+
+def kir(t_nods_quantity, x_nods_quantity, grid, transfer_velocity, time_step, x_step):
+    sigma = transfer_velocity * time_step / x_step
+    for n in range(1, t_nods_quantity - 1):
+        for m in range(1, x_nods_quantity - 1):
+            grid[n][m] = grid[n - 1][m] - sigma * (grid[n - 1][m + 1] - grid[n - 1][m])
+    return grid
