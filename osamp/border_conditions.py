34e3743d3dcdff9fa51ba390e5ce26111bb1f652
Rinat Aynulin <rinataynulin@gmail.com>
2017-05-13 21:05:56 +0300

    update: apply border conditions extending existing grid

--- osamp/border_conditions.py
+++ osamp/border_conditions.py
@@ -35,62 +35,59 @@ def border_condition_1d(grid, type_of_task, border_left, border_right):
 
 
 def border_condition_1d_acoustic(grid, border_left, border_right):
-    grid_copy = grid[:]
-
+    grid_new = []
     # Check left border.
     if border_left == ConditionNames.REFLECTION_CONDITION:
-        grid_copy[0][p] = -grid[0][p]
-        grid_copy[0][v] = -grid[0][v]
+        for i in [2, 1, 0]: # todo add dependence on method
+            grid_new.append([-grid[i][p], -grid[i][v]])
 
-        grid_copy[1][p] = -grid[1][p]
-        grid_copy[1][v] = -grid[1][v]
+    # elif border_left == ConditionNames.CYCLE_CONDITION:
+    #     grid_copy[1][p] = grid[len(grid) - 1][p]
+    #     grid_copy[1][v] = grid[len(grid) - 1][v]
+    #
+    #     grid_copy[0][p] = grid[len(grid) - 2][p]
+    #     grid_copy[0][v] = grid[len(grid) - 2][v]
+    #
+    # elif border_left == ConditionNames.ABSORBING_CONDITION:
+    #     grid_copy[1][p] = 0
+    #     grid_copy[1][v] = 0
+    #
+    #     grid_copy[0][p] = 0
+    #     grid_copy[0][v] = 0
+    #
+    #     # else:
+    #     # todo add new methods
 
-    elif border_left == ConditionNames.CYCLE_CONDITION:
-        grid_copy[1][p] = grid[len(grid) - 1][p]
-        grid_copy[1][v] = grid[len(grid) - 1][v]
-
-        grid_copy[0][p] = grid[len(grid) - 2][p]
-        grid_copy[0][v] = grid[len(grid) - 2][v]
-
-    elif border_left == ConditionNames.ABSORBING_CONDITION:
-        grid_copy[1][p] = 0
-        grid_copy[1][v] = 0
-
-        grid_copy[0][p] = 0
-        grid_copy[0][v] = 0
-
-        # else:
-        # todo add new methods
-
+    grid_new.extend(grid[:])
     # Check right border.
     if border_right == ConditionNames.REFLECTION_CONDITION:
-        grid_copy[len(grid) - 1][p] = -grid[len(grid) - 1][p]
-        grid_copy[len(grid) - 1][v] = -grid[len(grid) - 1][v]
+        for i in [0, 1, 2]:
+            grid_new.append([-grid[len(grid) - (i + 1)][p], -grid[len(grid) - (i + 1)][v]])
 
-        grid_copy[len(grid) - 2][p] = -grid[len(grid) - 2][p]
-        grid_copy[len(grid) - 2][v] = -grid[len(grid) - 2][v]
+    # elif border_right == ConditionNames.CYCLE_CONDITION:
+    #     grid_copy[len(grid) - 1][p] = grid[1][p]
+    #     grid_copy[len(grid) - 1][v] = grid[1][v]
+    #
+    #     grid_copy[len(grid) - 2][p] = grid[0][p]
+    #     grid_copy[len(grid) - 2][v] = grid[0][v]
+    #
+    # elif border_right == ConditionNames.ABSORBING_CONDITION:
+    #     grid_copy[len(grid) - 1][p] = 0
+    #     grid_copy[len(grid) - 1][v] = 0
+    #
+    #     grid_copy[len(grid) - 2][p] = 0
+    #     grid_copy[len(grid) - 2][v] = 0
+    #
+    #     # else:
+    #     # todo add new methods
 
-    elif border_right == ConditionNames.CYCLE_CONDITION:
-        grid_copy[len(grid) - 1][p] = grid[1][p]
-        grid_copy[len(grid) - 1][v] = grid[1][v]
-
-        grid_copy[len(grid) - 2][p] = grid[0][p]
-        grid_copy[len(grid) - 2][v] = grid[0][v]
-
-    elif border_right == ConditionNames.ABSORBING_CONDITION:
-        grid_copy[len(grid) - 1][p] = 0
-        grid_copy[len(grid) - 1][v] = 0
-
-        grid_copy[len(grid) - 2][p] = 0
-        grid_copy[len(grid) - 2][v] = 0
-
-        # else:
-        # todo add new methods
-
-    return grid_copy
+    return grid_new
 
 
 def border_condition_1d_seismic(arr, border_left, border_right):
     # for now seismic and acoustic border conditions do the same thing, but it's not the rule.
     # fixme
     return border_condition_1d_acoustic(arr, border_left, border_right)
+
+
+# print border_condition_1d([[0, 1], [1, 2], [2, 3], [4, 5], [5, 6]], ProblemTypes.ACOUSTIC, ConditionNames.REFLECTION_CONDITION, ConditionNames.REFLECTION_CONDITION)
