d78f4c0610e290b621dc6a06749060be993344b0
Ilya Mogilin <ilya.mogilin@phystech.edu>
2017-05-13 04:03:20 +0300

    Updated beam_warming method. Now it processes one array for any fixed time step

--- utils/convection_diffusion_equation_solution/beam_warming.py
+++ utils/convection_diffusion_equation_solution/beam_warming.py
@@ -5,23 +5,10 @@ def get_border_cond_for_beam_warming(row):
     return [-row[0]] + row
 
 def beam_warming(transfer_velocity, tau, h, grid):
-    grid = grid.tolist()
-    ext_grid = []
-
-    for i in range(0, len(grid[0])):
-        ext_grid.append(get_border_cond_for_beam_warming(grid[i]))
-
-    print ext_grid
-    print len(ext_grid)
-    print len(ext_grid[0])
-
+    ext_grid = np.array(get_border_cond_for_beam_warming(grid.tolist()))
     sigma = transfer_velocity * tau / h
-    for rows in range(0, len(ext_grid)):
-        for cols in range(2, len(ext_grid[0]) - 1):
-            ext_grid[rows+1][cols] = ext_grid[rows][cols] - sigma * (ext_grid[rows][cols] - ext_grid[rows][cols-1]) + sigma/2 * (1 - sigma) * (ext_grid[rows][cols] - 2 * ext_grid[rows][cols-1] + ext_grid[rows][cols-2])
-        del ext_grid[rows][0]
-    ext_grid = np.array(ext_grid)
-    return ext_grid
-
-a = beam_warming(1, 0.1, 0.1, np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
-print a
+    result_grid = np.zeros(ext_grid.size)
+    for m in range(2, len(ext_grid)):
+        result_grid[m] = ext_grid[m] - sigma * (ext_grid[m] - ext_grid[m-1]) + sigma/2 * (1 - sigma) * (ext_grid[m] - 2 * ext_grid[m-1] + ext_grid[m-2])
+    result_grid = np.delete(result_grid, 0)
+    return result_grid
