b31484f3dd9ff7975c933e19eb294999db164ffa
Khakhulin <taras.khakhulin@phystech.edu>
2017-05-15 01:38:04 +0300

    fix: changed method

--- utils/convection_diffusion_equation_solution/beam_warming.py
+++ utils/convection_diffusion_equation_solution/beam_warming.py
@@ -1,27 +1,12 @@
 #!/usr/bin/python
 import numpy as np
 
-def get_border_cond_for_beam_warming(row):
-    return [-row[0]] + row
-
 def beam_warming(transfer_velocity, tau, h, grid):
-    grid = grid.tolist()
-    ext_grid = []
-
-    for i in range(0, len(grid[0])):
-        ext_grid.append(get_border_cond_for_beam_warming(grid[i]))
-
-    print ext_grid
-    print len(ext_grid)
-    print len(ext_grid[0])
-
+    #grid = grid.tolist()
+    ext_grid = grid
     sigma = transfer_velocity * tau / h
-    for rows in range(0, len(ext_grid)):
-        for cols in range(2, len(ext_grid[0]) - 1):
-            ext_grid[rows+1][cols] = ext_grid[rows][cols] - sigma * (ext_grid[rows][cols] - ext_grid[rows][cols-1]) + sigma/2 * (1 - sigma) * (ext_grid[rows][cols] - 2 * ext_grid[rows][cols-1] + ext_grid[rows][cols-2])
-        del ext_grid[rows][0]
+    for m in range(2, len(ext_grid) - 1):
+        grid[m] = grid[m] - np.dot(sigma ,(grid[m] - grid[m-1])) + np.dot(sigma/2, np.dot((1 - sigma) , (grid[m] - 2 * grid[m-1] + grid[m-2])))
+        #ext_grid[cols] = ext_grid[cols] - sigma * (ext_grid[cols] - ext_grid[cols-1]) + sigma/2 * (1 - sigma) * (ext_grid[cols] - 2 * ext_grid[cols-1] + ext_grid[cols-2])
     ext_grid = np.array(ext_grid)
-    return ext_grid
-
-a = beam_warming(1, 0.1, 0.1, np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
-print a
+    return grid[1:-1]
