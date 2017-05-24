32d3eb5ba2f012065ab3936a8ed1ddeb7f03bbb2
Ilya Mogilin <ilya.mogilin@phystech.edu>
2017-05-24 21:55:17 +0300

    Updated methods to receive an array of transfer_velocities. Added tests for some of them(launch plots.py to make graphs).

--- utils/convection_diffusion_equation_solution/kir.py
+++ utils/convection_diffusion_equation_solution/kir.py
@@ -2,11 +2,13 @@ import numpy as np
 """Use numpy arrays and lists"""
 
 def kir(x_nods_quantity, grid, transfer_velocity, time_step, x_step):
-    sigma = transfer_velocity * time_step / x_step
-    new_grid = np.zeros(grid.shape)
+    new_grid = grid
     for m in range(1, x_nods_quantity - 1):
-        if (transfer_velocity >= 0):
-            new_grid[m] = grid[m] - sigma * (grid[m] - grid[m-1])
+        if (transfer_velocity[0] >= 0):
+            sigma = transfer_velocity[m] * time_step / x_step
+            new_grid[m] = grid[m] - np.dot(sigma, (grid[m] - grid[m-1]))
+            continue
         else:
-            new_grid[m] = grid[m] - sigma * (grid[m+1] - grid[m])
-    return new_grid[1:-1]
+            new_grid[m] = grid[m] - np.dot(sigma, (grid[m+1] - grid[m]))
+    print(new_grid)
+    return new_grid
